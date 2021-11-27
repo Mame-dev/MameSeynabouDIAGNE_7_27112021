31b6fabe4379f54ff2f490ece512108878c6d123
Mame-dev <mameseynaboudiagne@yahoo.fr>
2021-11-27 18:23:52 +0100

    Modification App.vue, Main.js, ajout router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header id="header">
+      <img class="logo" alt="Vue logo" src="#" />
+    </header>
+    <div id="conn-connected"></div>
+    <router-view />
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      conn: "",
+    };
+  },
+  created() {
+    axios
+      .get(this.$localhost + "api/")
+      .then((response) => (this.conn = response.data))
+      //.catch(error => console.log('Il semblerait que la base de données ne soit pas connectée, réessayez plus tard'))
+      .catch((error) => {
+        console.log(error);
+        document.getElementById("conn-connected").innerHTML =
+          "/!\\ Attention la database n'est pas connectée veuillez réessayer ultérieurement";
+      });
+  },
+};
 </script>
 
 <style>
@@ -21,8 +34,41 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#header {
+  text-align: center;
+}
+#nav {
+  padding: 0px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.logo {
+  width: 60%;
+  margin: 0;
+  text-align: right;
+}
+.navigation {
+  text-align: left;
+}
+.btn-primary {
+  background-color: #1956c8 !important;
+  font-weight: 600;
+}
+#conn-connected {
+  text-align: center;
+  font-size: 24px;
+  color: red;
+}
+@media (min-width: 1024px) {
+  .logo {
+    width: 20%;
+  }
 }
 </style>
